26b0fd71b404925078b72b44f7795605c3f9beae
Jacob <jisawyer@iastate.edu>
2015-05-28 15:18:13 -0500

    Added exercises 1-3

---
+++ select-from-nobel.sql
@@ -0,0 +1,78 @@
+/*
+Third section of sqlzoo, SELECT from Nobel
+*/
+
+
+
+
+--#1
+SELECT yr, subject, winner
+FROM nobel
+WHERE yr = 1950
+
+--#2
+SELECT winner
+FROM nobel
+WHERE yr = 1962 AND subject = 'Literature'
+
+--#3
+SELECT yr, subject
+FROM nobel
+WHERE winner = 'Albert Einstein'
+
+--#4
+SELECT winner
+FROM nobel
+WHERE subject = 'Peace' AND yr >= 2000
+
+--#5
+SELECT yr, subject, winner
+FROM nobel
+WHERE (yr >=1980 AND yr <=1989) AND subject = 'Literature'
+
+--#6
+SELECT *
+FROM nobel
+WHERE winner IN ('Theodore Roosevelt', 'Woodrow Wilson', 'Jimmy Carter')
+
+--#7
+SELECT winner
+FROM nobel
+WHERE winner LIKE 'john%'
+
+--#8
+SELECT *
+FROM nobel
+WHERE (subject = "Physics" AND yr = '1980') OR (subject = 'Chemistry' AND yr = 1984)
+
+--#9
+SELECT *
+FROM nobel
+WHERE yr = 1980 AND subject NOT IN ('Chemistry', 'Medicine')
+
+--#10
+SELECT *
+FROM nobel
+WHERE (subject  = 'Medicine' AND yr < 1910) OR (subject = 'Literature' AND yr >= 2004)
+
+--#11
+SELECT *
+FROM nobel
+WHERE winner LIKE 'peter gr%nberg'
+
+--#12
+SELECT *
+FROM nobel
+WHERE winner = 'Eugene O''Neill'
+
+--#13
+SELECT winner, yr, subject
+FROM nobel
+WHERE winner LIKE 'sir%'
+ORDER BY yr DESC, winner
+
+--#14
+SELECT winner, subject, subject IN ('Physics','Chemistry')
+FROM nobel
+WHERE yr=1984
+ORDER BY subject IN ('Physics','Chemistry'),subject,winner
---
+++ select-from-world.sql
@@ -0,0 +1,79 @@
+/*
+Second section of sqlzoo, SELECT from WORLD
+*/
+
+
+--#1
+SELECT name, continent, population
+FROM world
+
+--#2
+SELECT name
+FROM world
+WHERE population>=200000000
+
+--#3
+SELECT name, gdp/population
+FROM world
+WHERE population >= 200000000
+
+--#4
+SELECT name, population/1000000
+FROM world
+WHERE continent = 'South America'
+
+--#5
+SELECT name, population
+FROM world
+WHERE name in ('France', 'Germany', 'Italy')
+
+--#6
+SELECT name
+FROM world
+WHERE name LIKE '%united%'
+
+--#7
+SELECT name, population, area
+FROM world
+WHERE area > 3000000 OR population > 250000000
+
+--#8
+SELECT name, population, area
+FROM world
+WHERE (area > 3000000 AND population < 250000000)
+  OR (area < 3000000 and population > 250000000)
+
+--#9
+SELECT name, ROUND(population/1000000,2), ROUND(gdp/1000000000, 2)
+FROM world
+WHERE continent = 'South America'
+
+--#10
+SELECT name, ROUND(gdp/population, -3)
+FROM world
+WHERE gdp > 1000000000000
+
+--#11
+SELECT name, continent,
+       CASE WHEN continent='Caribbean' THEN 'North America'
+            ELSE continent END
+FROM world
+WHERE name LIKE 'J%'
+
+--#12
+SELECT name,
+CASE WHEN continent = 'Europe' OR continent = 'Asia' THEN 'Eurasia'
+      WHEN continent LIKE '%america%' OR continent = 'Caribbean' THEN 'America'
+      ELSE continent END
+FROM world
+WHERE name LIKE 'a%' or name LIKE 'b%'
+
+--#13
+SELECT name, continent,
+CASE WHEN continent = 'Oceania' THEN 'Australasia'
+     WHEN continent = 'Eurasia' OR name = 'Turkey' THEN 'Europe/Asia'
+     WHEN continent = 'Caribbean' AND name LIKE 'b%' THEN 'North America'
+     WHEN continent = 'Caribbean' AND name NOT LIKE 'b%' THEN 'South America'
+     ELSE continent END
+FROM world
+ORDER BY name
