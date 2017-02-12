ffa92bbe28d9c0c9447b86dd97a5334e59377c7d
Jan Kvapil <quapka@gmail.com>
2017-02-12 18:06:25 +0100

    Solution for SUM and COUNT with nobel table

---
+++ sum-and-count-nobel.sql
@@ -0,0 +1,63 @@
+/*
+The nobel table can be used to practice more SUM and COUNT functions.
+*/
+
+--#1
+/*
+Show the total number of prizes awarded. 
+*/
+SELECT COUNT(winner) FROM nobel
+
+--#2
+/*
+List each subject - just once 
+*/
+SELECT DISTINCT subject
+FROM nobel
+
+--#3
+/*
+Show the total number of prizes awarded for Physics. 
+*/
+SELECT COUNT(*) FROM nobel WHERE subject='Physics'
+
+--#4
+/*
+For each subject show the subject and the number of prizes. 
+*/
+SELECT subject, COUNT(*)
+FROM nobel
+GROUP BY subject
+
+--#5
+/*
+For each subject show the first year that the prize was awarded. 
+*/
+SELECT subject,MIN(yr)
+FROM nobel
+GROUP BY subject
+
+--#6
+/*
+For each subject show the number of prizes awarded in the year 2000. 
+*/
+SELECT subject, COUNT(winner)
+FROM nobel
+WHERE yr=2000
+GROUP BY subject
+
+--#7
+/*
+Show the number of different winners for each subject. 
+*/
+SELECT DISTINCT subject, COUNT(DISTINCT winner)
+FROM nobel
+GROUP BY subject
+
+--#8
+/*
+For each subject show how many years have had prizes awarded. 
+*/
+SELECT subject, COUNT(DISTINCT yr)
+FROM nobel
+GROUP BY subject
