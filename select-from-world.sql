116ff09ad8f99bf83ef02c0f43dbc191f67a77c6
Lisa Hull <lisa@jchull.com>
2017-02-23 10:33:02 -0700

    off by one?

--- select-from-world.sql
+++ select-from-world.sql
@@ -90,7 +90,7 @@ Show per-capita GDP for the trillion dollar countries to the nearest $1000.
 */
 SELECT name, ROUND(gdp/population, -3)
 FROM world
-WHERE gdp > 1000000000000
+WHERE gdp >= 1000000000000
 
 --#11
 /*
